9c61da49d01fda61076c7ec6f76afc2b9a17c32b
Noël Sigmunczyk <65312133+linlogge@users.noreply.github.com>
2026-05-01 16:17:53 +0200

    Remove error conversion from host_function

--- examples/host-function/src/main.rs
+++ examples/host-function/src/main.rs
@@ -1,12 +1,12 @@
-use rjsi::{Args, CallbackCx, DefaultRuntime, Engine, JsError, JsResult, Runtime, Value};
+use rjsi::{Args, CallbackCx, DefaultRuntime, Engine, JsResult, Runtime, Value};
 
 fn add<'cx, 'rt, E: Engine>(
     cx: &mut CallbackCx<'cx, 'rt, E>,
     _this: Value<'rt, E>,
     args: Args<'rt, E>,
 ) -> JsResult<'rt, E, Value<'rt, E>> {
-    let a_val = args.get(0).ok_or(JsError::type_err("missing argument"))?;
-    let b_val = args.get(1).ok_or(JsError::type_err("missing argument"))?;
+    let a_val = args.get(0).unwrap();
+    let b_val = args.get(1).unwrap();
 
     let a = a_val.to_f64(cx.cx())? as i32;
     let b = b_val.to_f64(cx.cx())? as i32;
