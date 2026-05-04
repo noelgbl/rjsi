1492d6f1819d6494226f029a881dbab8986cf608
Noël Sigmunczyk <65312133+linlogge@users.noreply.github.com>
2026-05-04 15:24:50 +0200

    Modify example to use rjsi::console instead of direct crate

--- examples/console/src/main.rs
+++ examples/console/src/main.rs
@@ -1,11 +1,10 @@
-use rjsi::{DefaultRuntime, Runtime};
-use rjsi_console::init;
+use rjsi::{DefaultRuntime, Runtime, console};
 
 fn main() {
     let mut runtime = DefaultRuntime::default();
 
     runtime.with_scope(|cx| {
-        init(cx).unwrap();
+        console::init(cx).unwrap();
 
         cx.eval("console.log('Hello from RJSI');").unwrap();
     });
