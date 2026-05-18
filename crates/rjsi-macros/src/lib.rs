4db79cf6f26433d87a3eb4bc397aaa4fb072b9fd
Noël Sigmunczyk <65312133+linlogge@users.noreply.github.com>
2026-05-18 08:47:25 +0200

    Implement case manipulation in FromJs/IntoJs

--- crates/rjsi-macros/src/lib.rs
+++ crates/rjsi-macros/src/lib.rs
@@ -4,6 +4,111 @@ use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
 use syn::{Data, DeriveInput, Fields, Index, ItemImpl, parse_macro_input};
 
+use heck::{
+    ToKebabCase, ToLowerCamelCase, ToShoutyKebabCase, ToShoutySnakeCase, ToSnakeCase,
+    ToUpperCamelCase,
+};
+
+#[derive(Clone, Copy, Default)]
+enum RenameAll {
+    #[default]
+    None,
+    LowerCamelCase,
+    UpperCamelCase,
+    SnakeCase,
+    ScreamingSnakeCase,
+    KebabCase,
+    ScreamingKebabCase,
+    Lowercase,
+    Uppercase,
+}
+
+impl RenameAll {
+    fn from_lit(s: &str) -> Option<Self> {
+        match s {
+            "camelCase" => Some(Self::LowerCamelCase),
+            "PascalCase" => Some(Self::UpperCamelCase),
+            "snake_case" => Some(Self::SnakeCase),
+            "SCREAMING_SNAKE_CASE" => Some(Self::ScreamingSnakeCase),
+            "kebab-case" => Some(Self::KebabCase),
+            "SCREAMING-KEBAB-CASE" => Some(Self::ScreamingKebabCase),
+            "lowercase" => Some(Self::Lowercase),
+            "UPPERCASE" => Some(Self::Uppercase),
+            _ => None,
+        }
+    }
+
+    fn apply(self, s: &str) -> String {
+        match self {
+            Self::None => s.to_string(),
+            Self::LowerCamelCase => s.to_lower_camel_case(),
+            Self::UpperCamelCase => s.to_upper_camel_case(),
+            Self::SnakeCase => s.to_snake_case(),
+            Self::ScreamingSnakeCase => s.to_shouty_snake_case(),
+            Self::KebabCase => s.to_kebab_case(),
+            Self::ScreamingKebabCase => s.to_shouty_kebab_case(),
+            Self::Lowercase => s.to_lowercase(),
+            Self::Uppercase => s.to_uppercase(),
+        }
+    }
+}
+
+fn js_nested_metas(
+    attrs: &[syn::Attribute],
+) -> impl Iterator<Item = syn::Meta> + use<'_> {
+    attrs
+        .iter()
+        .filter(|a| a.path().is_ident("js"))
+        .flat_map(|a| {
+            a.meta
+                .require_list()
+                .and_then(|list| {
+                    list.parse_args_with(
+                        syn::punctuated::Punctuated::<syn::Meta, syn::Token![,]>::parse_terminated,
+                    )
+                })
+                .into_iter()
+                .flatten()
+                .collect::<Vec<_>>()
+        })
+}
+
+fn parse_rename_all(attrs: &[syn::Attribute]) -> RenameAll {
+    for meta in js_nested_metas(attrs) {
+        if let syn::Meta::NameValue(nv) = meta {
+            if nv.path.is_ident("rename_all") {
+                if let syn::Expr::Lit(el) = &nv.value {
+                    if let syn::Lit::Str(s) = &el.lit {
+                        if let Some(conv) = RenameAll::from_lit(&s.value()) {
+                            return conv;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    RenameAll::None
+}
+
+fn parse_field_rename(attrs: &[syn::Attribute]) -> Option<String> {
+    for meta in js_nested_metas(attrs) {
+        if let syn::Meta::NameValue(nv) = meta {
+            if nv.path.is_ident("rename") {
+                if let syn::Expr::Lit(el) = &nv.value {
+                    if let syn::Lit::Str(s) = &el.lit {
+                        return Some(s.value());
+                    }
+                }
+            }
+        }
+    }
+    None
+}
+
+fn field_key(field_attrs: &[syn::Attribute], rust_name: &str, rename_all: RenameAll) -> String {
+    parse_field_rename(field_attrs).unwrap_or_else(|| rename_all.apply(rust_name))
+}
+
 mod class;
 
 pub(crate) fn core_path() -> TokenStream2 {
@@ -74,13 +179,13 @@ pub fn js_skip(_attr: TokenStream, item: TokenStream) -> TokenStream {
     item
 }
 
-#[proc_macro_derive(IntoJs)]
+#[proc_macro_derive(IntoJs, attributes(js))]
 pub fn derive_into_js(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     expand_into_js(&input).into()
 }
 
-#[proc_macro_derive(FromJs)]
+#[proc_macro_derive(FromJs, attributes(js))]
 pub fn derive_from_js(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     expand_from_js(&input).into()
@@ -107,12 +212,13 @@ fn runtime_path() -> TokenStream2 {
 fn expand_into_js(input: &DeriveInput) -> TokenStream2 {
     let path = runtime_path();
     let ident = &input.ident;
+    let rename_all = parse_rename_all(&input.attrs);
     match &input.data {
         Data::Struct(data) => match &data.fields {
             Fields::Named(fields) => {
                 let setters = fields.named.iter().map(|field| {
                     let name = field.ident.as_ref().unwrap();
-                    let key = name.to_string();
+                    let key = field_key(&field.attrs, &name.to_string(), rename_all);
                     quote! {
                         object.set_typed(cx, #key, self.#name)?;
                     }
@@ -171,12 +277,13 @@ fn expand_into_js(input: &DeriveInput) -> TokenStream2 {
 fn expand_from_js(input: &DeriveInput) -> TokenStream2 {
     let path = runtime_path();
     let ident = &input.ident;
+    let rename_all = parse_rename_all(&input.attrs);
     let from_js_impl = match &input.data {
         Data::Struct(data) => match &data.fields {
             Fields::Named(fields) => {
                 let readers = fields.named.iter().map(|field| {
                     let name = field.ident.as_ref().unwrap();
-                    let key = name.to_string();
+                    let key = field_key(&field.attrs, &name.to_string(), rename_all);
                     quote! {
                         #name: object.get_typed(cx, #key)?
                     }
